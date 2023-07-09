f111a624314e92efc3c97747d1b3f3a761a54321
DanielAndresSmg <danielandressmg@gmail.com>
2023-07-09 12:12:40 +0700

    tahap awal

---
+++ src/app/budget-item-list/budget-item-card/budget-item-card.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-budget-item-card',
+  templateUrl: './budget-item-card.component.html',
+  styleUrls: ['./budget-item-card.component.scss'],
+})
+export class BudgetItemCardComponent implements OnInit {
+  // @Input() isIncome: boolean;
+  @Input() isIncome: boolean = false;
+  constructor() {}
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+  }
+}
